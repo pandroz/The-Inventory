d1a1ae6b658fda31a670b2737620fbb39742d972
pandroz <combi.alessandro.a@gmail.com>
2026-02-18 22:11:39 +0100

    [0.0.36] Removed wrong logic to change user active status

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -1,8 +1,5 @@
-const User = require('../models/user');
-
 module.exports = (req, res, next) => {
     if (!req.session.userId) {
-        User.setActiveStatus(false);
         req.flash('sessionExpiredError', 'Sessione scaduta. Per favore, effettua nuovamente il login.');
         return res.redirect('/login');
     } else 
